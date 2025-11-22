582df5634163b3b5e8132f725aac595a249c4c89
mahbubur-r <mahbubur-r@outlook.com>
2025-11-22 02:38:00 +0100

    added our services and how it works in home page

--- src/pages/Home/Brands/Brands.jsx
+++ src/pages/Home/Brands/Brands.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'swiper/css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Marquee from 'react-fast-marquee';
 import amazon from '../../../assets/brands/amazon.png'
 import amazon_vector from '../../../assets/brands/amazon_vector.png'
 import casio from '../../../assets/brands/casio.png'
@@ -15,25 +16,41 @@ const brandLogo = [amazon, amazon_vector, casio, moonstar, randstad, star, start
 
 const Brands = () => {
     return (
-        <Swiper
-            slidesPerView={4}
-            centeredSlides={true}
-            spaceBetween={30}
-            loop={true}
-            grabCursor={true}
-            autoplay={{
-                delay: 2000,
-                disableOnInteraction: false,
-            }}
-            modules={[Autoplay]}
-        >
-            {
-                brandLogo.map((logo, index) => <SwiperSlide key={index}><img src={logo} /></SwiperSlide>)
-            }
+        <section className="py-12 overflow-hidden">
+            <div className="container mx-auto">
+                <h2 className="text-3xl font-bold text-center mb-10">We've helped thousands of sales teams</h2>
+                <div className="relative w-full">
+                    {/* <div className="flex gap-8 animate-slide"> */}
+                    <Marquee gradient={false} loop={0} speed={30} pauseOnHover={false}>
+                        {
+                            brandLogo.map((logo, index) => (
+                                <div key={index} className="flex-shrink-0 mx-5 md:mx-10 xl:mx-20">
+                                    <img src={logo} alt={`Client ${index + 1}`} className="" />
+                                </div>
+                            ))
+                        }
+                    </Marquee>
+                    {/* </div> */}
+                </div>
+            </div>
+        </section>
+        // <Swiper
+        //     slidesPerView={4}
+        //     centeredSlides={true}
+        //     spaceBetween={30}
+        //     loop={true}
+        //     grabCursor={true}
+        //     autoplay={{
+        //         delay: 2000,
+        //         disableOnInteraction: false,
+        //     }}
+        //     modules={[Autoplay]}
+        // >
+        //     {
+        //         brandLogo.map((logo, index) => <SwiperSlide key={index}><img src={logo} /></SwiperSlide>)
+        //     }
+        // </Swiper>
 
-
-
-        </Swiper>
     );
 };
 
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Brands from '../Brands/Brands';
 import Reviews from '../Reviews/Reviews';
+import HowItWorks from '../HowItWorks/HowItWorks';
+import OurServices from '../OurServices/OurServices';
 
 const reviewsPromise = fetch('/reviews.json').then(res => res.json());
 
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
+            <Banner></Banner>  
+            <OurServices></OurServices>
+            <HowItWorks></HowItWorks>
             <Brands></Brands>
             <Reviews reviewsPromise= {reviewsPromise}></Reviews>
         </div>
